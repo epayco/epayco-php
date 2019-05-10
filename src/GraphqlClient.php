ef8dc1ba2fb47a4ecb9c1a75dbe93018c2858785
juanhurtadopayco <juan.hurtado@payco.co>
2019-05-10 12:08:26 -0500

    feat(query validations added)
    validate selector, wildCard, byDates, pagination, customFields

--- src/GraphqlClient.php
+++ src/GraphqlClient.php
@@ -12,28 +12,30 @@ use Epayco\Exceptions\ErrorException;
  */
 class GraphqlClient
 {
-  public function validate($query){
+    public function validate($query){
 
-      //Inicializar parametros requeridos para wrapper
-      $action = $query->action;
-      $selector = $query->selector;
-      $selectorOr = $query->selectorOr;
-      $wildCard = $query->wildCard;
-      $byDates = $query->byDates;
+     //Inicializar parametros requeridos para wrapper
+     $action = $query->action;
+     $selector = $query->selector;
+     $selectorOr = $query->selectorOr;
+     $wildCard = $query->wildCard;
+     $byDates = $query->byDates;
+     $pagination = $query->pagination;
+     $customFields = $query->customFields;
 
-    //Comprobacion: El query tiene un action: find o findOne?
-      if (!($query->action === "find" || $query->action === "findOne")) {
-        throw new ErrorException("Parameter required, please specify action: find or findOne and try again.",102);
-      }
+     //Comprobacion: El query tiene un action: find o findOne?
+     if (!($query->action === "find" || $query->action === "findOne")) {
+         throw new ErrorException("Parameter required, please specify action: find or findOne and try again.",102);
+     }
 
-    //Comprobacion: El query tiene un atributo selector o selectorOr, si el action es igual a "find" el atributo puede ser nulo, este caso es para listar todos los registros de un modelo     
-    if (  $selector === null && $selectorOr === null) {
+     //Comprobacion: El query tiene un atributo selector o selectorOr, si el action es igual a "find" el atributo puede ser nulo, este caso es para listar todos los registros de un modelo
+     if (  $selector === null && $selectorOr === null) {
       throw new ErrorException("Parameter required, selector is empty or invalid please fill and try again.",103);
-    }else{
+     }else{
       if ($selector !== null && count($selector) > 0){
           throw new ErrorException("Parameter required, please specify action: find or findOne and try again.",103);
       }
-    }
+     }
 
     //Comprobacion: El query requiere un comodin de busqueda
     if ($wildCard !== null) {
@@ -51,18 +53,37 @@ class GraphqlClient
         if (gettype($byDates) !== "array"){
             throw  new ErrorException("Parameter required, byDates is empty or invalid please fill and try again.",105);
         }else{
-            echo "comprobar fechas";
-            echo $d = $byDates["start"];
             if (!($this->validateDateFormat($byDates["start"],'YYYY-MM-DD')
                 && $this->validateDateFormat($byDates["end"],'YYYY-MM-DD')
                 ) ) {
                 throw  new ErrorException("Parameter required, byDates is empty or invalid please fill and try again.",105);
-            }else{
-                echo "fechas correctas";
             }
         }
-    }else{
-        echo "no requiere fecha";
+    }
+
+      //Comprobación: El query solicita una paginacion de registros
+    if ($pagination !== null) {
+          if (gettype($pagination) !== "array") {
+              throw  new ErrorException("Parameter required, pagination is empty or invalid please fill and try again.",106);
+          }else{
+              if($pagination["limit"] === null || $pagination["pageNumber"] === null){
+                  throw new ErrorException("Parameter required, pagination limit or pageNumber is empty or invalid please fill and try again.",106);
+              }else{
+                  if (gettype($pagination["limit"]) !== "integer" || gettype($pagination["pageNumber"]) !== "integer") {
+                      throw new ErrorException("Parameter required, pagination limit or pageNumber has a invalid value type please fill and try again.",106);
+                  }
+              }
+        }
+    }
+     //Comprobación: El query solicita campos personalizados
+    if ($customFields !== null) {
+        if (gettype($customFields) !== "string") {
+            throw new ErrorException("Parameter required, customFields is empty or invalid please fill and try again.",107);
+        }else{
+            if( empty($customFields)){
+                throw new ErrorException("Parameter required, customFields is empty or invalid please fill and try again.",107);
+            }
+        }
     }
 
       /* $query = '
@@ -122,10 +143,12 @@ class GraphqlClient
         return json_decode($response->body,true);
     }
 
-    public function validateDateFormat($date, $format = 'Y-m-d'){
-        $d = date_create_from_format($format, $date);
-        // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
-        return $d &&  date_format($date, $format) === $date;
+    public function validateDateFormat($date){
+        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$date)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     
