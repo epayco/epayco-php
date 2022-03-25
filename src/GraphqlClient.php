86cd6250d866288ca39540a5463f5aeaf2eefb2f
Gerson Vasquez <gerson.vasquez@epayco.com>
2022-03-24 20:43:32 -0400

    fix(timeout): added timeout options for requests in graphQL petitions

--- src/GraphqlClient.php
+++ src/GraphqlClient.php
@@ -102,7 +102,12 @@ class GraphqlClient
                 'query' => $query
             ];
 
-            $response = Requests::post(Client::BASE_URL . '/graphql', $headers, $body);
+            $options = array(
+                'timeout' => 120,
+                'connect_timeout' => 120,
+            );
+
+            $response = Requests::post(Client::BASE_URL . '/graphql', $headers, $body, $options);
 
         } catch (\Throwable $th) {
             return $th->getMessage();
