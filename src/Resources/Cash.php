1fce8993126dfd9e757bba753d099c088362eedc
Gerson Vasquez <gerson.vasquez@epayco.com>
2022-05-25 12:00:56 -0400

    fix(efectivo): Se consulta medios de pagos activos

--- src/Resources/Cash.php
+++ src/Resources/Cash.php
@@ -16,41 +16,37 @@ class Cash extends Resource
      * @param  String $options data transaction
      * @return object
      */
-    public function create($type = null, $options = null)
+    public function create($type = null)
     {
-        $url = null;
-        switch ($type) {
-            case "efecty":
-                $url = "/restpagos/v2/efectivo/efecty";
-                break;
-            case "baloto":
-                $url = "/restpagos/v2/efectivo/baloto";
-                break;
-            case "gana":
-                $url = "/restpagos/v2/efectivo/gana";
-                break;
-            case 'redservi':
-                $url = "/restpagos/v2/efectivo/redservi";
-                break;
-            case 'puntored':
-                $url = "/restpagos/v2/efectivo/puntored";
-                break;
-            case 'sured':
-                $url = "/restpagos/v2/efectivo/sured";
-                break;
-            case 'apostar':
-                $url = "/restpagos/v2/efectivo/apostar";
-                break;
-            case 'susuerte':
-                $url = "/restpagos/v2/efectivo/susuerte";
-                break;
-            default:
-                throw new ErrorException($this->epayco->lang, 109);
-                break;
+
+        $methods_payment = $this->request(
+            "GET",
+            "/payment/cash/entities",
+            $api_key = $this->epayco->api_key,
+            null,
+            $private_key = $this->epayco->private_key,
+            $test = $this->epayco->test,
+            $switch = false,
+            $lang = $this->epayco->lang,
+            $cash = false,
+            false,
+            true
+        );
+        if(!isset($methods_payment->data) && !is_array($methods_payment->data)){
+            throw new ErrorException($this->epayco->lang, 106);
         }
+        $options = array_map(function($item){
+            return strtolower($item->name);
+        }, $methods_payment->data);
+
+        if(!in_array(strtolower($type),  $options)){
+            throw new ErrorException($this->epayco->lang, 109);
+
+        }
+        
         return $this->request(
                 "POST",
-                $url,
+                "/restpagos/v2/efectivo/{$type}",
                 $api_key = $this->epayco->api_key,
                 $options,
                 $private_key = $this->epayco->private_key,
