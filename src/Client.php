ef1c5b7a6d2f9e2bb8cbc16f6d86861e322c50aa
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-05-16 15:05:45 -0500

    fix:Se ajusta endpoint de rest pagos

--- src/Client.php
+++ src/Client.php
@@ -15,7 +15,7 @@ class Client extends GraphqlClient
 {
 
     const BASE_URL = "https://eks-subscription-api-lumen-service.epayco.io";
-    const BASE_URL_SECURE = "https://eks-rest-recaudo-service.epayco.io/restpagos";
+    const BASE_URL_SECURE = "https://eks-rest-pagos-service.epayco.io/restpagos";
     const BASE_URL_APIFY = "https://eks-apify-service.epayco.io";
     const IV = "0000000000000000";
     const LENGUAGE = "php";
