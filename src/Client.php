9872d663b6a195c813ca476547dbbee97d3ae284
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-09-18 16:04:12 -0500

    feat: cambio de endpoint a produccion

--- src/Client.php
+++ src/Client.php
@@ -14,10 +14,10 @@ use WpOrg\Requests\Requests;
 class Client extends GraphqlClient
 {
 
-    const BASE_URL = "https://eks-subscription-api-lumen-service.epayco.io";
-    const BASE_URL_SECURE = "https://eks-rest-pagos-service.epayco.io";
+    const BASE_URL = "https://api.secure.payco.co";
+    const BASE_URL_SECURE = "https://secure.payco.co";
     const ENTORNO = "/restpagos";
-    const BASE_URL_APIFY = "https://eks-apify-service.epayco.io";
+    const BASE_URL_APIFY = "https://apify.epayco.co";
     const IV = "0000000000000000";
     const LENGUAGE = "php";
 
